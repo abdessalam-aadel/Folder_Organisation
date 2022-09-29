50d34943410d71335e417eb6e9a51cda7b92d826
abdessalam-aadel <abdessalam.aadel@gmail.com>
2022-09-29 21:00:29 +0100

    First Commit

--- Folder_Organisation/Form1.Designer.cs
+++ Folder_Organisation/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtBoxFolder1 = new System.Windows.Forms.TextBox();
+            this.txtBoxFolder2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtGit = new System.Windows.Forms.Label();
+            this.txtBoxLoad = new System.Windows.Forms.TextBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.txtDone = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnStart.Location = new System.Drawing.Point(219, 42);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 49);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtBoxFolder1
+            // 
+            this.txtBoxFolder1.Location = new System.Drawing.Point(67, 42);
+            this.txtBoxFolder1.Name = "txtBoxFolder1";
+            this.txtBoxFolder1.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxFolder1.TabIndex = 1;
+            this.txtBoxFolder1.Text = "IF68";
+            // 
+            // txtBoxFolder2
+            // 
+            this.txtBoxFolder2.Location = new System.Drawing.Point(67, 71);
+            this.txtBoxFolder2.Name = "txtBoxFolder2";
+            this.txtBoxFolder2.Size = new System.Drawing.Size(100, 20);
+            this.txtBoxFolder2.TabIndex = 2;
+            this.txtBoxFolder2.Text = "IF85E";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folder 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Folder 2:";
+            // 
+            // txtGit
+            // 
+            this.txtGit.AutoSize = true;
+            this.txtGit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.txtGit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGit.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txtGit.Location = new System.Drawing.Point(138, 94);
+            this.txtGit.Name = "txtGit";
+            this.txtGit.Size = new System.Drawing.Size(38, 13);
+            this.txtGit.TabIndex = 5;
+            this.txtGit.Text = "Github";
+            this.txtGit.Click += new System.EventHandler(this.txtGit_Click);
+            // 
+            // txtBoxLoad
+            // 
+            this.txtBoxLoad.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtBoxLoad.Location = new System.Drawing.Point(16, 16);
+            this.txtBoxLoad.Name = "txtBoxLoad";
+            this.txtBoxLoad.Size = new System.Drawing.Size(201, 20);
+            this.txtBoxLoad.TabIndex = 6;
+            this.txtBoxLoad.Text = "Chose your folder ...";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoad.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnLoad.Location = new System.Drawing.Point(219, 16);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 20);
+            this.btnLoad.TabIndex = 7;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // txtDone
+            // 
+            this.txtDone.AutoSize = true;
+            this.txtDone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDone.ForeColor = System.Drawing.Color.Maroon;
+            this.txtDone.Location = new System.Drawing.Point(235, 92);
+            this.txtDone.Name = "txtDone";
+            this.txtDone.Size = new System.Drawing.Size(0, 17);
+            this.txtDone.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(298, 110);
+            this.Controls.Add(this.txtDone);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.txtBoxLoad);
+            this.Controls.Add(this.txtGit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxFolder2);
+            this.Controls.Add(this.txtBoxFolder1);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Organisation";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtBoxFolder1;
+        private System.Windows.Forms.TextBox txtBoxFolder2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label txtGit;
+        private System.Windows.Forms.TextBox txtBoxLoad;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Label txtDone;
     }
 }
 
--- Folder_Organisation/Form1.cs
+++ Folder_Organisation/Form1.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Folder_Organisation
 {
@@ -14,6 +10,189 @@ namespace Folder_Organisation
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void txtGit_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://github.com/abdessalam-aadel");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog FD = new FolderBrowserDialog();
+            if (FD.ShowDialog() == DialogResult.OK)
+            {
+                txtBoxLoad.Text = FD.SelectedPath;
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string path = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            txtBoxLoad.Text = path;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            txtDone.Text = "";
+            if (txtBoxLoad.Text == "")
+            {
+                return;
+            }
+
+            // Creat Folder Result (IF68 && IF85E)
+
+            string pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string Result = pathDesktop + @"\Result";
+            if (!Directory.Exists(Result))
+            {
+                Directory.CreateDirectory(Result);
+            }
+
+            string R68 = pathDesktop + @"\Result\R68";
+            if (!Directory.Exists(R68))
+            {
+                Directory.CreateDirectory(R68);
+            }
+
+            string R85 = pathDesktop + @"\Result\R85";
+            if (!Directory.Exists(R85))
+            {
+                Directory.CreateDirectory(R85);
+            }
+
+            string[] allFolder = Directory.GetDirectories(txtBoxLoad.Text);
+
+            string creatCopy68 = "";
+            string creatCopy85 = "";
+
+            foreach (string folderpath in allFolder)
+            {
+                DirectoryInfo difolder = new DirectoryInfo(folderpath);
+                string[] subFolder = Directory.GetDirectories(folderpath);
+                foreach (string subfolderpath in subFolder)
+                {
+                    DirectoryInfo disub = new DirectoryInfo(subfolderpath);
+                    string[] files = Directory.GetFiles(subfolderpath);
+
+                    // ==> IF68
+                    if (disub.Name == txtBoxFolder1.Text)
+                    {
+                        if (files.Length == 0)
+                        {
+                            creatCopy68 = "0";
+                        }
+                        else
+                        {
+                            creatCopy68 = "1";
+                        }
+                    }
+                    else { }
+
+                    // ==> IF85E
+                    if (disub.Name == txtBoxFolder2.Text)
+                    {
+                        if (files.Length == 0)
+                        {
+                            creatCopy85 = "0";
+                        }
+                        else
+                        {
+                            creatCopy85 = "1";
+                        }
+                    }
+                    else { }
+
+                    // Creat Copy folder to Result Folder
+                    if (creatCopy68 == "1" && creatCopy85 == "1")
+                    {
+                        // D'ont Copy Folder
+                        creatCopy68 = "";
+                        creatCopy85 = "";
+                    }
+
+                    else if (creatCopy68 == "1" && creatCopy85 == "0")
+                    {
+                        CopyDirectory(folderpath, R68 + @"\" + difolder.Name, true);
+                        creatCopy68 = "";
+                        creatCopy85 = "";
+                    }
+
+                    else if (creatCopy68 == "0" && creatCopy85 == "1")
+                    {
+                        CopyDirectory(folderpath, R85 + @"\" + difolder.Name, true);
+                        creatCopy68 = "";
+                        creatCopy85 = "";
+                    }
+
+                    else if (creatCopy68 == "0" && creatCopy85 == "0")
+                    {
+                        CopyDirectory(folderpath, Result + @"\" + difolder.Name, true);
+                        creatCopy68 = "";
+                        creatCopy85 = "";
+                    }
+                }
+            }
+            txtDone.Text = "Done!";
+        }
+
+        // Methode Copy Folder
+        //static public void CopyFolder(string sourceFolder, string destFolder)
+        //{
+        //    if (!Directory.Exists(destFolder))
+        //        Directory.CreateDirectory(destFolder);
+        //    string[] files = Directory.GetFiles(sourceFolder);
+        //    foreach (string file in files)
+        //    {
+        //        string name = Path.GetFileName(file);
+        //        string dest = Path.Combine(destFolder, name);
+        //        File.Copy(file, dest);
+        //    }
+        //    string[] folders = Directory.GetDirectories(sourceFolder);
+        //    foreach (string folder in folders)
+        //    {
+        //        string name = Path.GetFileName(folder);
+        //        string dest = Path.Combine(destFolder, name);
+        //        CopyFolder(folder, dest);
+        //    }
+        //}
+
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            // Get information about the source directory
+            var dir = new DirectoryInfo(sourceDir);
+
+            // Check if the source directory exists
+            if (!dir.Exists)
+                throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+            // Cache directories before we start copying
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            // Create the destination directory
+            Directory.CreateDirectory(destinationDir);
+
+            // Get the files in the source directory and copy to the destination directory
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath,true);
+            }
+
+            // If recursive and copying subdirectories, recursively call this method
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
         }
     }
 }
