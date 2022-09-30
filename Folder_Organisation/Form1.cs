ad9287dfde81a59f5ec62d208f5e8ab59cd9100a
abdessalam-aadel <abdessalam.aadel@gmail.com>
2022-09-30 21:10:54 +0100

    New update

--- Folder_Organisation/Form1.cs
+++ Folder_Organisation/Form1.cs
@@ -15,17 +15,15 @@ namespace Folder_Organisation
         private void txtGit_Click(object sender, EventArgs e)
         {
             // Go to Github repositorie
-            Process.Start("https://github.com/abdessalam-aadel");
+            Process.Start("https://github.com/abdessalam-aadel/Folder_Organisation");
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
         {
             // Load Folder to reorganize
             FolderBrowserDialog FD = new FolderBrowserDialog();
-            if (FD.ShowDialog() == DialogResult.OK)
-            {
+            if ( FD.ShowDialog() == DialogResult.OK )
                 txtBoxLoad.Text = FD.SelectedPath;
-            }
         }
 
         // Enable Drag-and-Drop folder to the Form Main
@@ -46,46 +44,45 @@ namespace Folder_Organisation
             {
                 // Clear the text Done!
                 txtDone.Text = "";
+                Cursor = Cursors.WaitCursor;
 
                 // check if the user has been selected a folder
                 // if not return ..
-                if (txtBoxLoad.Text == "")
-                {
+                if ( txtBoxLoad.Text == "" )
                     return;
-                }
                 
                 // get the path of Desktop
                 string pathDesktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
                 // Creat Folder Result, 68 and 85
                 string Result = pathDesktop + @"\Result";
-                if (!Directory.Exists(Result))
+                if ( !Directory.Exists(Result) )
                     Directory.CreateDirectory(Result);
 
                 string R68 = pathDesktop + @"\Result\R68";
-                if (!Directory.Exists(R68))
+                if ( !Directory.Exists(R68) )
                     Directory.CreateDirectory(R68);
 
                 string R85 = pathDesktop + @"\Result\R85";
-                if (!Directory.Exists(R85))
+                if ( !Directory.Exists(R85) )
                     Directory.CreateDirectory(R85);
-
-                // Creat Folder Probleme
-                string prb = pathDesktop + @"\Result\PRB";
-                if (!Directory.Exists(prb))
-                    Directory.CreateDirectory(prb);
 
                 // Get all folder
                 string[] allFolder = Directory.GetDirectories(txtBoxLoad.Text);
 
                 // creating two variable to check if the 
-                // folder IF68 and IF85E is empty or not
-                string creatCopy68 = "";
-                string creatCopy85 = "";
+                // folder 1 and 2 is empty or not
+                string creatCopy1 = "";
+                string creatCopy2 = "";
 
                 // Starting the boucle for-each in All Folder
-                foreach (string folderpath in allFolder)
+                foreach ( string folderpath in allFolder )
                 {
+                    // Creating two variables to check if the Folder folder 1 
+                    // and folder 2 Exist or not
+                    bool checkExisteF1 = Directory.Exists(folderpath + @"\" + txtBoxFolder1.Text);
+                    bool checkExisteF2 = Directory.Exists(folderpath + @"\" + txtBoxFolder2.Text);
+
                     // Creating Directory Info to get the name of the folder path
                     DirectoryInfo difolder = new DirectoryInfo(folderpath);
 
@@ -93,7 +90,7 @@ namespace Folder_Organisation
                     string[] subFolder = Directory.GetDirectories(folderpath);
 
                     // Starting the boucle for-each in All Sub-Folder
-                    foreach (string subfolderpath in subFolder)
+                    foreach ( string subfolderpath in subFolder )
                     {
                         // Creating Directory Info to get the name of the sub-folder path
                         DirectoryInfo disub = new DirectoryInfo(subfolderpath);
@@ -101,99 +98,123 @@ namespace Folder_Organisation
                         // Get all Files in sub-folder
                         string[] files = Directory.GetFiles(subfolderpath);
 
-                        // Check if the folder name equal to ==> IF68
-                        if (disub.Name == txtBoxFolder1.Text)
+                        // Check if the folder name equal to name of folder 1
+                        if ( disub.Name == txtBoxFolder1.Text )
                         {
-                            // Check if the folder (IF68) is Empty or not
-                            if (files.Length == 0)
-                                creatCopy68 = "0";
+                            // Check if the folder 1 is Empty or not
+                            if ( files.Length == 0 )
+                                creatCopy1 = "0";
                             else
-                                creatCopy68 = "1";
+                                creatCopy1 = "1";
                         }
 
-                        // Check if the folder name equal to ==> IF85E
-                        if (disub.Name == txtBoxFolder2.Text)
+                        // Check if the folder name equal to name of folder 2
+                        if ( disub.Name == txtBoxFolder2.Text )
                         {
-                            // Check if the folder (IF85E) is Empty or not
-                            if (files.Length == 0)
-                                creatCopy85 = "0";
+                            // Check if the folder 2 is Empty or not
+                            if ( files.Length == 0 )
+                                creatCopy2 = "0";
                             else
-                                creatCopy85 = "1";
+                                creatCopy2 = "1";
                         }
 
                         // Start Copying Folder to Result Folder
                         // ...
-                        // Check if the folder : IF68 && IF85E is not empty
-                        if (creatCopy68 == "1" && creatCopy85 == "1")
+                        // Check if the folder : Folder 1 && Folder 2 is not empty
+                        if ( creatCopy1 == "1" && creatCopy2 == "1" )
                         {
-                            // D'ont Copy Folder
-                            creatCopy68 = "";
-                            creatCopy85 = "";
+                            // D'ont Copy Folder 
+                            // ......_*_.......
+                            // Clear variables
+                            creatCopy1 = "";
+                            creatCopy2 = "";
                         }
 
-                        // Check if the folder : IF68 is not empty && IF85E is empty
-                        else if (creatCopy68 == "1" && creatCopy85 == "0")
-                        {
-                            // Copying the folder into R68 folder
-                            CopyDirectory(folderpath, R68 + @"\" + difolder.Name, true);
-                            creatCopy68 = "";
-                            creatCopy85 = "";
-                        }
-
-                        // Check if the folder : IF68 is empty && IF85E is not empty
-                        else if (creatCopy68 == "0" && creatCopy85 == "1")
+                        // Check if the folder 1 is not empty && folder 2 is empty
+                        else if ( creatCopy1 == "1" && creatCopy2 == "0" )
                         {
                             // Copying the folder into R85 folder
                             CopyDirectory(folderpath, R85 + @"\" + difolder.Name, true);
-                            creatCopy68 = "";
-                            creatCopy85 = "";
+                            creatCopy1 = "";
+                            creatCopy2 = "";
                         }
 
-                        // Check if the folder : IF68 && IF85E is empty
-                        else if (creatCopy68 == "0" && creatCopy85 == "0")
+                        // Check if the folder 1 is empty && folder 2 is not empty
+                        else if ( creatCopy1 == "0" && creatCopy2 == "1" )
+                        {
+                            // Copying the folder into R85 folder
+                            CopyDirectory(folderpath, R68 + @"\" + difolder.Name, true);
+                            creatCopy1 = "";
+                            creatCopy2 = "";
+                        }
+
+                        // Check if the folder 1 && folder 2 is empty
+                        else if ( creatCopy1 == "0" && creatCopy2 == "0" )
                         {
                             // Copying the folder into Result folder
                             CopyDirectory(folderpath, Result + @"\" + difolder.Name, true);
-                            creatCopy68 = "";
-                            creatCopy85 = "";
+                            creatCopy1 = "";
+                            creatCopy2 = "";
                         }
 
-                        // Check if the folder : IF68 && IF85E those not existe
-                        else if ( !Directory.Exists(folderpath + @"\IF68") && !Directory.Exists(folderpath + @"\IF85E") )
+                        // Check if the folder 1 and 2 those not existe
+                        else if ( !checkExisteF1 && !checkExisteF2 )
                         {
                             // Copying the folder into Result folder
-                            CopyDirectory(folderpath, prb + @"\" + difolder.Name, true);
-                            creatCopy68 = "";
-                            creatCopy85 = "";
+                            CopyDirectory(folderpath, Result + @"\" + difolder.Name, true);
+                            creatCopy1 = "";
+                            creatCopy2 = "";
                         }
 
-                        // Check if the folder : IF68 && IF85E those not existe
-                        else if (!Directory.Exists(folderpath + @"\IF68"))
+                        // Check if the folder 1 not existe
+                        else if ( !checkExisteF1 )
                         {
-                            // Copying the folder into Result folder
-                            CopyDirectory(folderpath, prb + @"\" + difolder.Name, true);
-                            creatCopy68 = "";
-                            creatCopy85 = "";
+                            if ( creatCopy2 == "1" )
+                            {
+                                // Copying the folder into R68
+                                CopyDirectory(folderpath, R68 + @"\" + difolder.Name, true);
+                                creatCopy1 = "";
+                                creatCopy2 = "";
+                            }
+                            else if ( creatCopy2 == "0" )
+                            {
+                                // Copying the folder into Result folder
+                                CopyDirectory(folderpath, Result + @"\" + difolder.Name, true);
+                                creatCopy1 = "";
+                                creatCopy2 = "";
+                            }
                         }
 
-                        // Check if the folder : IF68 && IF85E those not existe
-                        else if (!Directory.Exists(folderpath + @"\IF85E"))
+                        // Check if the folder 2 not existe
+                        else if ( !checkExisteF2 )
                         {
-                            // Copying the folder into Result folder
-                            CopyDirectory(folderpath, prb + @"\" + difolder.Name, true);
-                            creatCopy68 = "";
-                            creatCopy85 = "";
+                            if ( creatCopy1 == "1" )
+                            {
+                                // Copying the folder into R85
+                                CopyDirectory(folderpath, R85 + @"\" + difolder.Name, true);
+                                creatCopy1 = "";
+                                creatCopy2 = "";
+                            }
+                            else if ( creatCopy1 == "0" )
+                            {
+                                // Copying the folder into Result folder
+                                CopyDirectory(folderpath, Result + @"\" + difolder.Name, true);
+                                creatCopy1 = "";
+                                creatCopy2 = "";
+                            }
                         }
                     }
                 }
 
                 // Message success ..
+                Cursor = Cursors.Default;
                 txtDone.Text = "Done!";
             }
 
             catch (Exception ex)
             {
                 // Show message of Any Exception
+                Cursor = Cursors.Default;
                 MessageBox.Show(ex.Message);
             }
         }
@@ -205,7 +226,7 @@ namespace Folder_Organisation
             var dir = new DirectoryInfo(sourceDir);
 
             // Check if the source directory exists
-            if (!dir.Exists)
+            if ( !dir.Exists )
                 throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
 
             // Cache directories before we start copying
@@ -215,16 +236,16 @@ namespace Folder_Organisation
             Directory.CreateDirectory(destinationDir);
 
             // Get the files in the source directory and copy to the destination directory
-            foreach (FileInfo file in dir.GetFiles())
+            foreach ( FileInfo file in dir.GetFiles() )
             {
                 string targetFilePath = Path.Combine(destinationDir, file.Name);
                 file.CopyTo(targetFilePath,true);
             }
 
             // If recursive and copying subdirectories, recursively call this method
-            if (recursive)
+            if ( recursive )
             {
-                foreach (DirectoryInfo subDir in dirs)
+                foreach ( DirectoryInfo subDir in dirs )
                 {
                     string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
                     CopyDirectory(subDir.FullName, newDestinationDir, true);
